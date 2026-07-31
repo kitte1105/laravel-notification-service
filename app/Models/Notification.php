3597667f39a00ce141e19bac8678c85d42279045
Ekaterina Davydova <kitte1105@gmail.com>
2026-07-31 17:01:38 +0400

    Errors founded by PHPStan were fixed.

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use App\Enums\NotificationChannel;
 use App\Enums\NotificationStatus;
+use Carbon\Carbon;
 use Database\Factories\NotificationFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property NotificationStatus $status
+ * @property NotificationChannel $channel
+ * @property int $attempts
+ * @property string|null $last_error
+ * @property Carbon|null $delivered_at
+ * @property Carbon|null $last_attempt_at
+ * @property Carbon|null $processing_started_at
+ */
 class Notification extends Model
 {
     /** @use HasFactory<NotificationFactory> */
@@ -36,6 +46,9 @@ class Notification extends Model
         return [
             'status' => NotificationStatus::class,
             'channel' => NotificationChannel::class,
+            'delivered_at' => 'datetime',
+            'last_attempt_at' => 'datetime',
+            'processing_started_at' => 'datetime',
         ];
     }
 
